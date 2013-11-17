31f9360795b20990ca438bf3a005cf0037dd176d
Yves Talboom <yves_talboom@peopleware.be>
2013-11-17 12:59:21 +0100

    Make person table look nicer

--- src/main/java/ui/PersonList.java
+++ src/main/java/ui/PersonList.java
@@ -1,6 +1,7 @@
 package ui;
 
 import com.vaadin.ui.Table;
+import container.PersonContainer;
 import vaadinapp.MyVaadinUI;
 
 /**
@@ -13,5 +14,7 @@ public class PersonList extends Table {
         setSizeFull();
         // set datasource
         setContainerDataSource(app.getDataSource());
+        setVisibleColumns(PersonContainer.NATURAL_COL_ORDER);
+        setColumnHeaders(PersonContainer.COL_HEADERS_ENGLISH);
     }
 }
